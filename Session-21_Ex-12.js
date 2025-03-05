08e3d012010fa47c092d3e620e135486c5bb7825
SonDao0205 <daotruongson2006@gmail.com>
2025-03-05 11:41:05 +0700

    ex

--- Session-21_Ex-12.js
+++ Session-21_Ex-12.js
@@ -1,4 +1,3 @@
 for (let i = 0; i < 10; i++) {
     console.log(`%cMau sac da duoc thay doi`,`color : rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`);
-    
 }
